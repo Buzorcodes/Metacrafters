39dd05fc55787ef64e8b4e344c3ca8acad3fc7cc
Buzorcodes <141915811+Buzorcodes@users.noreply.github.com>
2023-08-30 13:36:18 +0100

    update the code to contain require statement

--- Metacrafters.sol
+++ Metacrafters.sol
@@ -6,20 +6,29 @@ contract Metacrafters {
 // public variable
     string public  TOKEN_NAME = "Metacrafters";
     string public  TOKEN_ABBRV = "MTC";
-    uint256 public totalSupply;
+    uint256 public totaSupply;
 
     // mapping variable here
     mapping(address => uint256) public balanceOf;
+// DICTIONARY IN PYTHON
+// OBJECTS IN JAVASCRIPT
+// KEY - PAIR SYSTEM
 
     // mint function
     function mint(address walletAddress, uint256 _value) public {
-        totalSupply += _value;
+        totaSupply += _value;
         balanceOf[walletAddress] += _value;
     }
 
     // burn function
     function burn(address walletAddress, uint256 _value) public {
-            totalSupply -= _value;
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        // 
+        // if(balanceOf[msg.sender] >= _value) {
+        //     totaSupply -= _value;
+        //     balanceOf[walletAddress] -= _value; 
+        // }
+            totaSupply -= _value;
             balanceOf[walletAddress] -= _value;
     }
 }
